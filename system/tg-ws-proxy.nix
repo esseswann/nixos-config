a4ab6699f8765545498f0b4f0c75d2086ba93a96
jsus <a.lebedev@itcanfly.org>
2026-05-19 13:20:52 +0300

    No secret in tg-ws-proxy

--- system/tg-ws-proxy.nix
+++ system/tg-ws-proxy.nix
@@ -1,14 +1,11 @@
 { inputs, ... }:
 
-let
-  secrets = import ../secrets.nix;
-in
 {
   systemd.services.tg-ws-proxy = {
     description = "Telegram WebSocket Proxy";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${inputs.tg-ws-proxy.packages.x86_64-linux.default}/bin/tg-ws-proxy --secret ${secrets.TG_WS_PROXY_SECRET}";
+      ExecStart = "${inputs.tg-ws-proxy.packages.x86_64-linux.default}/bin/tg-ws-proxy";
       Restart = "always";
     };
   };
